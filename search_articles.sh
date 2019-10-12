f93dcd70059f9d8c26aaabc424f9b76a8c5960fe
Matthias Küch <mail@matthias-kuech.de>
2019-10-12 18:30:02 +0200

    Escape var

--- search_articles.sh
+++ search_articles.sh
@@ -4,7 +4,7 @@ if [[ -z "${domain}" ]]; then
     domain=$(cat "$alfred_workflow_cache/last_domain")
 else
     mkdir -p "$alfred_workflow_cache"
-    echo -n $domain > "$alfred_workflow_cache/last_domain"
+    echo -n "$domain" > "$alfred_workflow_cache/last_domain"
 fi
 
 http "https://$domain/api/v1/Search/List?query=$query&limit=25&minArticleQuality=10&batch=1&namespaces=0%2C14" | 
