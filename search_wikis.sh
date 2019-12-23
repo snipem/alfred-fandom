76ce47995307415184c4f9a745502d0ee3768e89
Matthias Küch <mail@matthias-kuech.de>
2019-12-23 14:02:52 +0100

    Include domain in result

--- search_wikis.sh
+++ search_wikis.sh
@@ -3,4 +3,4 @@
 
 http "https://community.fandom.com/api/v1/Wikis/ByString?string=$query&lang=en%2Cde&limit=25&expand=1&batch=1&includeDomain=true" |
 jq \
-'{ "items" : [ .items[] | select(.wam_score > 0) | {uid: .id, title: .name, arg: .domain, subtitle: (.topic + " " + (.wam_score|tostring)) }] }'
+'{ "items" : [ .items[] | select(.wam_score > 0) | {uid: .id, title: .name, arg: .domain, subtitle: (.topic + " " + .domain + " " + (.wam_score|tostring)) }] }'
