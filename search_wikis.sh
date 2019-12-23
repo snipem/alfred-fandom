05e806ec3e97c245f5a35cb7a7e8105438487f2b
Matthias Küch <mail@matthias-kuech.de>
2019-12-23 13:53:35 +0100

    Add run command

--- search_wikis.sh
+++ search_wikis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# run: query="Assassins Creed" search_wikis.sh
 
 http "https://community.fandom.com/api/v1/Wikis/ByString?string=$query&lang=en%2Cde&limit=25&expand=1&batch=1&includeDomain=true" |
 jq \
