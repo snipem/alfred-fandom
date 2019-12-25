aaaa0e6f829f215a6582b2ecb59e7f397f7289fc
Matthias Küch <mail@matthias-kuech.de>
2019-12-25 19:51:35 +0100

    Add optional language

--- search_wikis.sh
+++ search_wikis.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# run: query="Assassins Creed" search_wikis.sh
+# run: lang=en query="Dragon's Dogma" search_wikis.sh
 
-http "https://community.fandom.com/api/v1/Wikis/ByString?string=$query&lang=en%2Cde&limit=25&expand=1&batch=1&includeDomain=true" |
+if [[ -z "${lang}" ]]; then
+    lang="en"
+fi
+
+http "https://community.fandom.com/api/v1/Wikis/ByString?string=$query&lang=$lang%2Cde&limit=25&expand=1&batch=1&includeDomain=true" |
 jq \
-'{ "items" : [ .items[] | select(.wam_score > 0) | {uid: .id, title: .name, arg: .domain, subtitle: (.topic + " " + .domain + " " + (.wam_score|tostring)) }] }'
+'{ "items" : [ .items[] | select(.wam_score > 0) | {uid: .id, title: .name, arg: .domain, subtitle: (.topic + " " + .domain + " " + .language + " " + (.wam_score|tostring)) }] }'
